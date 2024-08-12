7dd5db0cd450dc917f11465820fc9f53f3e347c2
RaghuBN <raghu.astepahead@gmail.com>
2024-08-13 00:27:28 +0530

    FirstCommit

---
+++ src/JavaTopicesDemo/addsubmuldivinJAVA.java
@@ -0,0 +1,12 @@
+package JavaTopicesDemo;
+
+public class addsubmuldivinJAVA {
+
+	public static void main(String[] args) {
+		System.out.println(6+2);
+		System.out.println(6-2);
+		System.out.println(6*6);
+		System.out.println(6/2);
+	}
+
+}
